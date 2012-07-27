06c776db32ae48188125132c1bf7aadfc389685e
Christian Schuster <blackyle@me.com>
2012-07-27 23:53:46 +0200

    explicitly assigned numbers to directions; compute direction from radians angle

--- Flip/JCSHexDirection.h
+++ Flip/JCSHexDirection.h
@@ -7,12 +7,12 @@
 //
 
 typedef enum {
-    JCSHexDirectionE,
-    JCSHexDirectionNE,
-    JCSHexDirectionNW,
-    JCSHexDirectionW,
-    JCSHexDirectionSW,
-    JCSHexDirectionSE,
+    JCSHexDirectionE = 0,
+    JCSHexDirectionNE = 1,
+    JCSHexDirectionNW = 2,
+    JCSHexDirectionW = 3,
+    JCSHexDirectionSW = 4,
+    JCSHexDirectionSE = 5,
 } JCSHexDirection;
 
 #define JCSHexDirectionMin JCSHexDirectionE
@@ -20,3 +20,7 @@ typedef enum {
 
 #define JCSHexDirectionRowDelta(direction) ((direction) == JCSHexDirectionNE || (direction) == JCSHexDirectionNW) - ((direction) == JCSHexDirectionSW || (direction) == JCSHexDirectionSE)
 #define JCSHexDirectionColumnDelta(direction) ((direction) == JCSHexDirectionE || (direction) == JCSHexDirectionSE) - ((direction) == JCSHexDirectionW || (direction) == JCSHexDirectionNW)
+
+// direction for a given angle (in radians)
+// 0 is East, Pi/3 is Northeast, etc.
+#define JCSHexDirectionForAngle(angle) ((JCSHexDirection) (fmod(fmod((angle),2.0*M_PI)+2.0*M_PI+M_PI/6,2.0*M_PI) / (M_PI/3)))
