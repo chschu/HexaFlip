2b1aeee776ae56ef83ae0cf9b673694db8e495b2
Christian Schuster <blackyle@me.com>
2012-12-09 22:42:06 +0100

    use "static" keyword to avoid duplicate symbols

--- Flip/Source/UI/JCSFlipUIConstants.h
+++ Flip/Source/UI/JCSFlipUIConstants.h
@@ -7,13 +7,13 @@
 //
 
 // distance between cell centers in points
-const float JCS_FLIP_UI_CELL_SPACING_POINTS = 38.0;
+static const float JCS_FLIP_UI_CELL_SPACING_POINTS = 38.0;
 
 // empty border around the cells in the board layer, in points
 // used for layout (if the board is aligned to a border)
-const float JCS_FLIP_UI_BOARD_BORDER = 10.0;
+static const float JCS_FLIP_UI_BOARD_BORDER = 10.0;
 
 // distance in points that a touch must be dragged away from the center of the touched cell to be considered "outside" that cell
 // larger values avoid accidentially selecting a direction, but require a longer dragging distance
 // larget values may prohibit proper selection of a direction with the starting cell near a border
-const float JCS_FLIP_UI_DRAG_OUTSIDE_THRESHOLD = JCS_FLIP_UI_CELL_SPACING_POINTS;
+static const float JCS_FLIP_UI_DRAG_OUTSIDE_THRESHOLD = JCS_FLIP_UI_CELL_SPACING_POINTS;
