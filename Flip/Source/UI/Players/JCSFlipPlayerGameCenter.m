ba0a26040ab04bff2c8733ba924563f46cca6e18
Christian Schuster <blackyle@me.com>
2013-01-24 23:40:11 +0100

    set outcomes and end the match if the game is over

--- Flip/Source/UI/Players/JCSFlipPlayerGameCenter.m
+++ Flip/Source/UI/Players/JCSFlipPlayerGameCenter.m
@@ -33,20 +33,51 @@
     // generate data for game center
     NSData *data = [gameCenterManager buildDataFromGameState:state];;
     
-    // find participant corresponding to non-local player (= this player)
+    // find participant corresponding to this player (non-local) and opponent (local)
     NSString *localPlayerID = gameCenterManager.localPlayerID;
     GKTurnBasedMatch *currentMatch = gameCenterManager.currentMatch;
     NSUInteger participantIndex = [currentMatch.participants indexOfObjectPassingTest:^BOOL(GKTurnBasedParticipant *obj, NSUInteger idx, BOOL *stop) {
         return ![localPlayerID isEqualToString:obj.playerID];
     }];
     GKTurnBasedParticipant *participant = [currentMatch.participants objectAtIndex:participantIndex];
+    GKTurnBasedParticipant *opponent = [currentMatch.participants objectAtIndex:1-participantIndex];
     
-    // TODO set outcomes and invoke endMatchInTurnWithMatchData on the match
-    
-    // end the turn, updating the match data
-    [currentMatch endTurnWithNextParticipant:participant matchData:data completionHandler:^(NSError *error) {
-        NSLog(@"%@", error);
-    }];
+    if (JCSFlipGameStatusIsOver(state.status)) {
+        // set outcomes before ending the match
+        if (state.status == JCSFlipGameStatusDraw) {
+            participant.matchOutcome = GKTurnBasedMatchOutcomeTied;
+            opponent.matchOutcome = GKTurnBasedMatchOutcomeTied;
+        } else {
+            // check if this player (non-local) is player A
+            BOOL isPlayerA = state.playerToMove == JCSFlipPlayerToMoveA;
+            if (state.status == JCSFlipGameStatusPlayerAWon) {
+                // if we're player A, we won
+                participant.matchOutcome = isPlayerA ? GKTurnBasedMatchOutcomeWon : GKTurnBasedMatchOutcomeLost;
+                opponent.matchOutcome = isPlayerA ? GKTurnBasedMatchOutcomeLost : GKTurnBasedMatchOutcomeWon;
+            } else {
+                // if we're player B, we lost
+                participant.matchOutcome = isPlayerA ? GKTurnBasedMatchOutcomeLost : GKTurnBasedMatchOutcomeWon;
+                opponent.matchOutcome = isPlayerA ? GKTurnBasedMatchOutcomeWon : GKTurnBasedMatchOutcomeLost;
+            }
+        }
+        
+        // end the match, updating the match data
+        [currentMatch endMatchInTurnWithMatchData:data completionHandler:^(NSError *error) {
+            if (error != nil) {
+                // TODO we need to retry or leave the game screen in this case
+                NSLog(@"%@", error);
+            }
+        }];
+        
+    } else {
+        // end the turn, updating the match data
+        [currentMatch endTurnWithNextParticipant:participant matchData:data completionHandler:^(NSError *error) {
+            if (error != nil) {
+                // TODO we need to retry or leave the game screen in this case
+                NSLog(@"%@", error);
+            }
+        }];
+    }
 }
 
 - (void)tellMakeMove:(JCSFlipGameState *)state {
