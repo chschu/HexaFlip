9ff161cbe28e81e883302588823c236e7d9d24b8
Christian Schuster <blackyle@me.com>
2013-01-26 01:21:38 +0100

    rename directories

--- HexaFlip/Source/main.m
+++ HexaFlip/Source/main.m
@@ -6,11 +6,9 @@
 //  Copyright Christian Schuster 2012. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
 int main(int argc, char *argv[]) {
-    
     @autoreleasepool {
         int retVal = UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
         return retVal;
