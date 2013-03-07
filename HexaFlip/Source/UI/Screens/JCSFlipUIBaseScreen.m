b0e0a69855c5da01ab68a9d96155df801a916a2a
Christian Schuster <blackyle@me.com>
2013-03-07 23:10:36 +0100

    don't render invisible screens

--- HexaFlip/Source/UI/Screens/JCSFlipUIBaseScreen.m
+++ HexaFlip/Source/UI/Screens/JCSFlipUIBaseScreen.m
@@ -13,4 +13,20 @@
 @synthesize screenPoint;
 @synthesize screenEnabled = _screenEnabled;
 
+- (void)visit {
+    CGSize winSize = [CCDirector sharedDirector].winSize;
+    
+    // determine screen's bounds in world coordinates
+    CGPoint p1 = [self convertToWorldSpace:ccp(0,0)];
+    CGPoint p2 = [self convertToWorldSpace:ccp(winSize.width,winSize.height)];
+    CGRect screenRectInWorldSpace = CGRectMake(p1.x,p1.y,p2.x-p1.x,p2.y-p1.y);
+    
+    // render this screen only if its screen rectangle intersects with the visible world rectangle
+    CGRect visibleRectInWorldSpace = CGRectMake(0, 0, winSize.width, winSize.height);
+    CGRect intersection = CGRectIntersection(visibleRectInWorldSpace, screenRectInWorldSpace);
+    if (!CGRectIsEmpty(intersection)) {
+        [super visit];
+    }
+}
+
 @end
