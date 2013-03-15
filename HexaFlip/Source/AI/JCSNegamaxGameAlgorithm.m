a81deaa034200db1133cadca43787b751781d4a3
Christian Schuster <blackyle@me.com>
2013-03-15 22:53:15 +0100

    collect principal variation (move sequence leading to the root node's minimax value)

--- HexaFlip/Source/AI/JCSNegamaxGameAlgorithm.m
+++ HexaFlip/Source/AI/JCSNegamaxGameAlgorithm.m
@@ -16,8 +16,8 @@
     // the move
     id move;
     
-    // the child's heuristic value (used for move ordering)
-    float childHeuristicValue;
+    // the sort criterion for move ordering
+    float criterion;
 }
 
 - (NSComparisonResult)compareTo:(JCSNegamaxChildData *)other;
@@ -27,8 +27,8 @@
 @implementation JCSNegamaxChildData
 
 - (NSComparisonResult)compareTo:(JCSNegamaxChildData *)other {
-    float v1 = self->childHeuristicValue;
-    float v2 = other->childHeuristicValue;
+    float v1 = self->criterion;
+    float v2 = other->criterion;
     if (v1 < v2) {
         return NSOrderedAscending;
     }
@@ -70,42 +70,57 @@
 }
 
 - (id)moveAtNode:(id<JCSGameNode>)node {
-    id bestMove = nil;
     _node = node;
-    
     _count = 0;
+    NSMutableArray *pv = [[NSMutableArray alloc] initWithCapacity:_depth];
     NSDate *start = [NSDate date];
     
-    float score = [self negamaxWithDepth:_depth alpha:-INFINITY beta:INFINITY bestMoveHolder:&bestMove currentHeuristicValue:[_heuristic valueOfNode:node]];
+    float score = [self negamaxWithDepth:_depth alpha:-INFINITY beta:INFINITY principalVariation:pv];
     
-    NSLog(@"analyzed %u nodes in %.3f seconds, got best move %@ with score %.3f%@",
-          _count, [[NSDate date] timeIntervalSinceDate:start], bestMove, score, _canceled ? @" (canceled)" : @"");
+    NSLog(@"analyzed %u nodes in %.3f seconds, got principal variation [%@] with score %.3f%@",
+          _count, [[NSDate date] timeIntervalSinceDate:start], [self stringForPrincipalVariation:pv], score, _canceled ? @" (canceled)" : @"");
     
-    return bestMove;
+    return [pv count] > 0 ? [pv objectAtIndex:0] : nil;
 }
 
-- (float)negamaxWithDepth:(NSUInteger)depth alpha:(float)alpha beta:(float)beta bestMoveHolder:(id *)bestMoveHolder currentHeuristicValue:(float)heuristicValue {
+- (NSString *)stringForPrincipalVariation:(NSArray *)principalVariation {
+    NSMutableString *result = [[NSMutableString alloc] init];
+    for (id move in principalVariation) {
+        if (result.length > 0) {
+            [result appendString:@", "];
+        }
+        [result appendString:[move description]];
+    }
+    return result;
+}
+
+- (float)negamaxWithDepth:(NSUInteger)depth alpha:(float)alpha beta:(float)beta principalVariation:(NSMutableArray *)principalVariation {
     _count++;
-	id bestMove = nil;
     
     if (depth > 0 && !_node.leaf) {
         @autoreleasepool {
             NSArray *entries = [self sortedChildrenAscending];
+            NSMutableArray *pv = [[NSMutableArray alloc] initWithCapacity:depth-1];
+            BOOL first = YES;
             for (JCSNegamaxChildData *entry in entries) {
                 
                 [_node pushMove:entry->move];
-                float score = -[self negamaxWithDepth:depth-1 alpha:-beta beta:-alpha bestMoveHolder:nil currentHeuristicValue:entry->childHeuristicValue];
+                float score = -[self negamaxWithDepth:depth-1 alpha:-beta beta:-alpha principalVariation:pv];
                 [_node popMove];
                 
                 if (score > alpha) {
-                    bestMove = entry->move;
+                    first = NO;
+                    [principalVariation setArray:pv];
+                    [principalVariation insertObject:entry->move atIndex:0];
                     alpha = score;
                     if (alpha >= beta) {
                         break;
                     }
-                } else if (bestMove == nil) {
+                } else if (first) {
                     // keep first move, just in case there are only really bad moves
-                    bestMove = entry->move;
+                    first = NO;
+                    [principalVariation setArray:pv];
+                    [principalVariation insertObject:entry->move atIndex:0];
                 }
                 
                 // check for cancellation
@@ -114,14 +129,9 @@
                 }
             }
         }
-        NSAssert(bestMove != nil, @"must have a best move");
     } else {
         // maximum depth reached, or leaf node - take the node's heuristic value
-        alpha = heuristicValue;
-    }
-    
-    if (bestMoveHolder != nil) {
-        *bestMoveHolder = bestMove;
+        alpha = [_heuristic valueOfNode:_node];
     }
     
     return alpha;
@@ -133,7 +143,7 @@
     [_node applyAllPossibleMovesAndInvokeBlock:^(id move, BOOL *stop) {
         JCSNegamaxChildData *entry = [[JCSNegamaxChildData alloc] init];
         entry->move = move;
-        entry->childHeuristicValue = [_heuristic valueOfNode:_node];
+        entry->criterion = [_heuristic valueOfNode:_node];
         [result addObject:entry];
     }];
     
