4af9a30f52d8d4a8a9b66282ecac8f8d0e42cde9
Christian Schuster <blackyle@me.com>
2013-05-26 23:08:34 +0200

    fixed GameCenter compatibility version instead of extraction from git tags

---
+++ HexaFlip/Support/InfoPlist-template.h
@@ -0,0 +1,8 @@
+// GameCenter compatibility version; used as CFBundleVersion, which is used to determine GameCenter compatibility (at least in the Sandbox)
+#define GAMECENTER 1
+
+// App version; used as CFBundleShortVersionString
+#define VERSION @VERSION@
+
+// Revision; used for display in the app
+#define REVISION @REVISION@
