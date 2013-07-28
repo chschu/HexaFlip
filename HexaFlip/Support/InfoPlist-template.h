a9a8542db59c26cc8ac68eecba300b2f8b0bc0a5
Christian Schuster <blackyle@me.com>
2013-07-28 20:09:44 +0200

    increase build number before release

--- HexaFlip/Support/InfoPlist-template.h
+++ HexaFlip/Support/InfoPlist-template.h
@@ -1,5 +1,6 @@
 // GameCenter compatibility version; used as CFBundleVersion, which is used to determine GameCenter compatibility (at least in the Sandbox)
-#define GAMECENTER 1
+// This value must be increased for each release. Remove all Sandbox matches before changing this value!
+#define GAMECENTER 2
 
 // App version; used as CFBundleShortVersionString
 #define VERSION @VERSION@
